df5df7cffbc5b761416145096c77366beea0f455
HaroldWSchranz <Harold.Schranz@gmail.com>
2022-12-02 13:09:08 +1100

    Figuring out Scores File IO

--- HS2020FIO/Assets/Scripts/SaveLoadScoreManager.cs
+++ HS2020FIO/Assets/Scripts/SaveLoadScoreManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SaveLoadScoreManager : MonoBehaviour
 {
+    public HighScores m_HighScores;
+
+    private float m_gameTime;
+    private bool isGameOver;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_gameTime = 0.0f;
+        isGameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_gameTime += Time.deltaTime;
+
+        if (isGameOver == true)
+        {
+            // save the score
+            m_HighScores.AddScore(Mathf.RoundToInt(m_gameTime));
+            m_HighScores.SaveScoresToFile();
+        }
     }
 }
